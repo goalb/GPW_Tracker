78fa7d5ecdf78da8cba13fa10abb2e05d99edf67
goalb <91460691+goalb@users.noreply.github.com>
2022-07-18 13:39:14 +0200

    Add files via upload

--- GPW_Tracker/GPW_Tracker/base/admin.py
+++ GPW_Tracker/GPW_Tracker/base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Company
+from .models import User
+from .models import Portfolio
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(User)
+admin.site.register(Portfolio)
+
--- GPW_Tracker/GPW_Tracker/base/models.py
+++ GPW_Tracker/GPW_Tracker/base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Company(models.Model):
+    name = models.CharField('name', max_length=60)
+    ticker = models.CharField('ticker', max_length=10)
+    website = models.URLField('Website', max_length=100)
+
+    def __str__(self):
+        return self.name + ' (' + self.ticker + ')'
+
+
+class User(models.Model):
+    login = models.CharField('login', max_length=30)
+    email = models.EmailField(max_length=200)
+    name = models.CharField('name', max_length=60, null=True)
+    registration_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.login
+
+
+class Portfolio(models.Model):
+    name = models.CharField('name', max_length=60)
+    company = models.ManyToManyField(Company, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
