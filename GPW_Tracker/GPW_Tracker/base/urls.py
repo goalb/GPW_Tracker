e2a4631c7c34933e087feca8c3bd8dba7bac8641
goalb <91460691+goalb@users.noreply.github.com>
2022-07-20 01:01:11 +0200

    Add files via upload

--- GPW_Tracker/GPW_Tracker/base/urls.py
+++ GPW_Tracker/GPW_Tracker/base/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add_portfolio', views.add_portfolio, name='add-portfolio'),
     path('list_portfolio', views.list_portfolio, name='list-portfolio'),
     path('show_portfolio/<portfolio_id>', views.show_portfolio, name='show-portfolio'),
+    path('show_company/<company_id>', views.show_company, name='show-company'),
+    path('search_company', views.search_company, name='search-company'),
 ]
--- GPW_Tracker/GPW_Tracker/base/views.py
+++ GPW_Tracker/GPW_Tracker/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import PortfolioForm
-from .models import Portfolio
+from .models import Portfolio, Company
 
 
 # from django.contrib.auth import authenticate, login, logout
@@ -36,6 +36,26 @@ def list_portfolio(request):
     return render(request, 'base/portfolio.html', {'show_portfolio': list_portfolio})
 
 
+def list_company(request):
+    list_company = Company.objects.all()
+    return render(request, 'base/company.html', {'show_company': list_company})
+
+
 def show_portfolio(request, portfolio_id):
     portfolio = Portfolio.objects.get(pk=portfolio_id)
     return render(request, 'base/show_portfolio.html', {'portfolio': portfolio})
+
+
+def show_company(request, company_id):
+    company = Company.objects.get(pk=company_id)
+    return render(request, 'base/show_company.html', {'company': company})
+
+
+def search_company(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        companies = Company.objects.filter(name__contains=searched)
+        return render(request, 'base/search_company.html', {'searched': searched, 'companies': companies})
+    else:
+        return render(request, 'base/search_company.html', {})
+
