dcd5fed1462f95964ee3dd3896aff7fb404e2b56
goalb <91460691+goalb@users.noreply.github.com>
2022-07-23 13:47:40 +0200

    Add files via upload

--- GPW_Tracker/GPW_Tracker/users/views.py
+++ GPW_Tracker/GPW_Tracker/users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 # Create your views here.
+
+# Everything with login/registration/logout
+
 
 def login_user(request):
     if request.method == "POST":
@@ -18,4 +23,28 @@ def login_user(request):
             messages.success(request, "Username or password is incorrect")
             return redirect('login')
     else:
-        return render(request, 'authenticate/login.html', {})
+        return render(request, 'registration/login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You were logged out")
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration successful"))
+            return redirect('home')
+    else:
+        form = RegisterUserForm()
+
+    return render(request, 'registration/register_user.html', {'form': form})
+
